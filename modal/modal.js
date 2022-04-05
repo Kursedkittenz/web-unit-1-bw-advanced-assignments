242a488cb760ae1a31f177ac9369ce719b089ad9
unknown <epeng1994@gmail.com>
2022-04-05 16:49:48 -0400

    simple modal completed

--- modal/modal.js
+++ modal/modal.js
@@ -1,1 +1,14 @@
 // you will need to toggle the visibility off and on with DOM selectors
+const open = document.getElementById('open')
+const unfolding_container = document.getElementById('unfoldingContainer')
+const close = document.getElementById('close')
+
+
+open.addEventListener('click',()=>{
+    unfolding_container.classList.add('show');
+})
+
+
+close.addEventListener('click',()=>{
+    unfolding_container.classList.remove('show');
+})
