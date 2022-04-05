f2ca208bfb46738d62677c0e7c8e73a5c39c6792
unknown <epeng1994@gmail.com>
2022-04-05 13:36:45 -0400

    created dark slider

--- dark-theme/dark-theme.js
+++ dark-theme/dark-theme.js
@@ -1,1 +1,10 @@
 // you need to create DOM selectors to grab the correct HTML elements and update their styles to the opposite theme's css classes when you want to toggle the theme off and on
+
+const checkbox = document.getElementById("checkbox")
+
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+//declare variable to point to input checkbox
+//addEventListener, 'change' to reassign class for body to .dark which is defined in CSS
